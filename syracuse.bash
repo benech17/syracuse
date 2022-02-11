581adbe68b2453dea70f9fbf05552bd2313d9938
Yaniv Benichou <38528800+benech17@users.noreply.github.com>
2022-02-11 11:44:16 +0100

    update script bash

--- syracuse.bash
+++ syracuse.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-function usage {
-    echo -e "\e[1mUsage:\e[0m\n\t$0 [-h|-c] [start] [end]\n"
-    echo -e "\e[1mOptions:\e[0m"
-    echo -e "\t\e[1m-h, --help\e[0m"
-    echo -e "\t\tPrint this help message and exit.\n"
-    echo -e "\t\e[1m-c, --clean\e[0m"
-    echo -e "\t\tClean the project directory. ! Warning ! All data files will be deleted.\n"
-    echo -e "\e[1mInformations:\e[0m"
-    echo -e "\t[start] and [end] must be strictly positive integer."
+#option -h,--help
+function print_help {
+    echo "Exemple d'execution:"
+    echo "          $0 [-h|-c] [début] [fin]"
+    echo "          où debut et fin doivent être des entiers positifs"
+    echo "Options:"
+    echo "          -h,--help : affiche une aide descriptive d'utilisation."
+    echo "          -c,--clean: Supprime les dossiers générés d'output"
     exit 0
 }
+
+#option -c,--clean
 function clean {
     if [ -d bin ] || [ -d Images ] || [ -d Synthesis ]; then        
         rm -rf bin/ Images/ Synthesis/
@@ -18,9 +19,11 @@ function clean {
     fi
     exit 0
 }
-function error_args {
+
+#print message d'erreur si il n'y a pas les bons arguments
+function erreur_parametres {
     echo "Veuillez entrer 2 arguments en ligne de commande."
-    echo "Exemple : ./syracuse.bash 5 15"
+    echo "Exemple : ./syracuse.bash 100 500"
     echo "Executez: ./syracuse.bash --help » pour avoir de l'aide"
     exit 0
 }
@@ -56,70 +59,76 @@ function collect_average {
 }
 
 
+# Fonction principale 
 
-# Check options
-
-# Si le nombre d'arguments = 0 ou >2 
+# On verifie que le nombre d'arguments =0 ou >2 
 if [ $# -eq 0 ] || [ $# -gt 2 ]; then
-    error_args
-elif [ $# -eq 1 ]; then 
-    # Si l'option d'aide ou de nettoyage est appelé
-    if [ $1 == "-h" ] || [ $1 == "--help" ]; then
-        usage
-    elif [ $1 == "-c" ] || [ $1 == "--clean" ]; then
-        clean
-    fi
+    erreur_parametres
+elif [ $# -eq 1 ]; then  
+    # switch sur les options
+    case $1 in
+    "-h") print_help;;
+    "--help") print_help;;
+    "-c") clean;;
+    "--clean") clean;;
+    esac
 fi
 
 
 isNumber='^[1-9]+[0-9]*$'  #regex pour savoir si c'est un nombre
 #on verifie si on a bien 2 arguments et ce sont 2 nombres
 if [ $# -eq 2 ] && [[ $1 =~ $isNumber ]] && [[ $2 =~ $isNumber ]]; then
-    # Création des répertoires
-    mkdir -p bin Images Synthesis
-    # compile le programme c si 
-    if [ "syracuse.c" -nt "bin/syracuse" ]; then
-        gcc syracuse.c -o bin/syracuse
-    fi
-    for i in $(seq $1 $2); do
+    mkdir --parents bin Images Synthesis # Création des répertoires
+    gcc syracuse.c -o bin/syracuse       # compile le programme c 
+
+    for i in $(seq $1 $2); do   # boucle entre  début et fin
         ./bin/syracuse ${i} bin/f${i}.dat
         # Collect data from data files and store them in temporary files
         collect_sequence_data bin/f${i}.dat sequence_data
         collect_altitude_max bin/f${i}.dat altitude_max ${i}
         collect_flight_time bin/f${i}.dat flight_time ${i}
         collect_altitude_time bin/f${i}.dat altitude_time ${i}
-    done   
-    # Analyze data with gnuplot
-    gnuplot -persist <<- EOFMarker
+    done  
+
+    # visualisation avec gnuplot
+    gnuplot -p <<- EOF
+        #vols.dat
         set terminal jpeg
-        set output "Images/vols[$1;$2].dat"
+        set output "Images/vols[$1;$2].jpeg"
         set title "Un en fonction de n pour tous les U0 dans [$1;$2]"
         set xlabel "n"
         set ylabel "Un"
-        plot "sequence_data" w l title "vols.dat"
+        plot "sequence_data" with lines title "vols.dat"
         reset
+
+        #altitude.dat
         set terminal jpeg
-        set output "Images/altitude[$1;$2].dat"
-        set title "Altitude maximum atteinte en fonction de U0"
-        set xlabel "U0"
+        set output "Images/altitude[$1;$2].jpeg"
+        set title "Altitude maximum atteinte en fonction de Uo"
+        set xlabel "Uo"
         set ylabel "Altitude maximum"
-        plot "altitude_max" w l title "altitude.dat"
+        plot "altitude_max" with lines title "altitude.dat"
         reset
+
+        #duréevol.dat
         set terminal jpeg
-        set output "Images/dureevol[$1;$2].dat"
-        set title "Duree de vol en fonction de U0"
-        set xlabel "U0"
+        set output "Images/dureevol[$1;$2].jpeg"
+        set title "Duree de vol en fonction de Uo"
+        set xlabel "Uo"
         set ylabel "Nombres d'occurrences"
-        plot "flight_time" w l title "dureevol.dat"
+        plot "flight_time" with lines title "dureevol.dat"
         reset
+
+        #dureealtitude.dat
         set terminal jpeg
-        set output "Images/dureealtitude[$1;$2].dat"
-        set title "Duree de vol en altitude en fonction de U0"
-        set xlabel "U0"
+        set output "Images/dureealtitude[$1;$2].jpeg"
+        set title "Duree de vol en altitude en fonction de Uo"
+        set xlabel "Uo"
         set ylabel "Nombres d'occurrences"
-        plot "altitude_time" w l title "dureealtitude.dat"
-EOFMarker
-    # Bonus : synthesis of all data
+        plot "altitude_time" with lines title "dureealtitude.dat"
+EOF
+
+    # Bonus : synthese des données
     maxU0=$(sort -k2n sequence_data | sed -n '$p' | cut -d' ' -f2)
     echo -e "Synthese Syracuse [$1;$2]\n" >> Synthesis/synthese-$1-$2.txt
     echo -e "altitude_max:" >> Synthesis/synthese-$1-$2.txt
@@ -137,5 +146,5 @@ EOFMarker
     # Remove temporary data files 
     rm bin/*.dat && rm sequence_data altitude_max flight_time altitude_time
 else
-    error_args
+    erreur_parametres
 fi
